812ae79ced749d830f8f38e35f5c174d2e9971e5
Martin Katov <48762563+Transcendence90@users.noreply.github.com>
2021-06-04 19:24:03 +0300

    Add Antiforgery in Startup

--- Web/MyCookingPassion.Web.ViewModels/Votes/PostVoteResultModel.cs
+++ Web/MyCookingPassion.Web.ViewModels/Votes/PostVoteResultModel.cs
@@ -2,6 +2,6 @@
 {
     public class PostVoteResultModel
     {
-        public double AverageVoteResult { get; set; }
+        public double AverageVote { get; set; }
     }
 }
--- Web/MyCookingPassion.Web/Controllers/VotesController.cs
+++ Web/MyCookingPassion.Web/Controllers/VotesController.cs
@@ -3,6 +3,7 @@
     using System.Security.Claims;
     using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
     using MyCookingPassion.Services.Data;
     using MyCookingPassion.Web.ViewModels.Votes;
@@ -19,13 +20,14 @@
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<ActionResult<PostVoteResultModel>> Post(PostVoteInputModel input)
         {
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
             await this.votesService.SetVoteAsync(input.RecipeId, userId, input.Value);
-            var averageVotesResult = this.votesService.GetAverageVotes(input.RecipeId);
+            var averageVotes = this.votesService.GetAverageVotes(input.RecipeId);
 
-            return new PostVoteResultModel { AverageVoteResult = averageVotesResult };
+            return new PostVoteResultModel { AverageVote = averageVotes };
         }
     }
 }
